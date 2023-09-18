8de2347834712d4bbb19b0d9098ef2ab76c9c5f7
zaktech96 <zakariyesahid96@gmail.com>
2023-09-18 15:08:41 +0100

    api implemented

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-// Import the Button component from the correct path
 import Reciters from "./components/Mainscreen";
-import List from "./components/ReciterShow";
 import Button from "./components/Button";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-// import ForestImage from "./Forest.jpg";
-// Import the RiPlayCircleFill icon from react-icons/ri
 import {
   RiSkipBackLine,
   RiPlayCircleLine,
@@ -14,72 +10,140 @@ import {
 } from "react-icons/ri";
 
 function App() {
-  const url = "https://jsonplaceholder.typicode.com/users";
-  const [data, setData] = useState([]);
-
-  const fetchInfo = () => {
-    return axios.get(url).then((res) => setData(res.data));
-  };
+  const [audioUrl, setAudioUrl] = useState("");
+  const [text, setText] = useState("");
+  const [surahs, setSurahs] = useState([]);
+  const [reciters, setReciters] = useState([]);
 
   useEffect(() => {
-    fetchInfo();
+    async function fetchData() {
+      try {
+        const surahEditionIdentifier = "en.asad";
+        const surahResponse = await axios.get(
+          `http://api.alquran.cloud/v1/quran/${surahEditionIdentifier}`
+        );
+
+        const { audioUrl, text } = surahResponse.data;
+        setAudioUrl(audioUrl);
+        setText(text);
+      } catch (error) {
+        console.error("An error occurred while fetching data:", error);
+        // You can handle the error here, e.g., display an error message to the user.
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const surahEditionIdentifier = "en.asad";
+        const surahResponse = await axios.get(
+          `http://api.alquran.cloud/v1/quran/${surahEditionIdentifier}`
+        );
+        const { audioUrl, text } = surahResponse.data;
+        setAudioUrl(audioUrl);
+        setText(text);
+      } catch (error) {
+        console.error("An error occurred while fetching data:", error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    async function fetchSurahs() {
+      try {
+        const surahsResponse = await axios.get(
+          "http://api.alquran.cloud/v1/surah"
+        );
+        const surahsData = surahsResponse.data.data;
+        setSurahs(surahsData);
+      } catch (error) {
+        console.error("An error occurred while fetching Surahs:", error);
+      }
+    }
+
+    fetchSurahs();
+  }, []);
+
+  useEffect(() => {
+    async function fetchReciters() {
+      try {
+        const recitersResponse = await axios.get(
+          "http://api.alquran.cloud/v1/reciters"
+        );
+        const recitersData = recitersResponse.data.data;
+        setReciters(recitersData);
+      } catch (error) {
+        console.error("An error occurred while fetching Reciters:", error);
+      }
+    }
+
+    fetchReciters();
   }, []);
 
   const handleClick = () => {
     console.log("played");
   };
-  // Define a click handler function for the first button
+
   const handleButtonClick1 = () => {
-    console.log("Button 1 was clicked"); // Log a message to the console when the button is clicked
+    console.log("Button 1 was clicked");
   };
 
-  // Define a click handler function for the second button
   const handleButtonClick2 = () => {
-    console.log("Button 2 was clicked"); // Log a message to the console when the button is clicked
+    console.log("Button 2 was clicked");
   };
 
-  // Define a click handler function for the third button
   const handleButtonClick3 = () => {
-    console.log("Button 3 was clicked"); // Log a message to the console when the button is clicked
+    console.log("Button 3 was clicked");
   };
 
-  // Render the App component
   return (
     <div className="flex flex-col justify-center items-center h-screen">
-      {/* Audio player */}
       <audio
         handleClick={handleClick}
         controls
-        className=" audio-player px-4 py-2 m-3"
+        className="audio-player px-4 py-2 m-3"
       >
-        <source src="horse.ogg" type="audio/ogg" />
-        <source src="horse.mp3" type="audio/mpeg" />
+        <source src={audioUrl} type="audio/mpeg" />
         Your browser does not support the audio element.
       </audio>
       <div className="flex">
-        {/* Render the first Button component and pass the click handler function */}
         <Button handleClick={handleButtonClick1}>
           <RiSkipBackLine className="inline-block mr-2 text-2xl" />
           rewind
         </Button>
-
-        {/* Render the second Button component and include the RiPlayCircleFill icon */}
         <Button handleClick={handleButtonClick2}>
           play
-          {/* Render the RiPlayCircleFill icon with inline-block display, margin, and text size */}
           <RiPlayCircleLine className="inline-block ml-3 text-2xl" />
         </Button>
-
-        {/* Render the third Button component and pass the click handler function */}
         <Button handleClick={handleButtonClick3}>
           Skip
           <RiSkipForwardLine className="inline-block ml-3 text-2xl" />
         </Button>
       </div>
+      <p>
+        Text: {text}
+        <br />
+        Surahs:
+        <ul>
+          {surahs.map((surah) => (
+            <li key={surah.number}>{surah.englishName}</li>
+          ))}
+        </ul>
+        Reciters:
+        <ul>
+          {reciters.map((reciter) => (
+            <li key={reciter.id}>{reciter.name}</li>
+          ))}
+        </ul>
+      </p>
       <Reciters />
-      {/* Audio player */}
     </div>
   );
 }
-// Export the App component
+
 export default App;
